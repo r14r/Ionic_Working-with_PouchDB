8ad7223ce539ec91c114cb6019d6405369116e2f
Ralph Goestenmeier <r14r@via-internet.de>
2020-08-31 14:42:59 +0200

    master | .

---
+++ App/src/app/services/my-notes/my-notes.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import PouchDB from 'pouchdb';
+
+
+@Injectable()
+export class MyNotesProvider {
+
+	private database: any;
+	private myNotes: any;
+
+	constructor() {
+		this.database = new PouchDB('my-notes');
+	}
+
+	public addNote(theNote: string): Promise<string> {
+		const promise = this.database
+			.put({
+				_id: ('note:' + (new Date()).getTime()),
+				note: theNote
+			})
+			.then((result): string => {
+				return (result.id);
+			});
+
+		return (promise);
+	}
+
+	getMyNotes() {
+		return new Promise(resolve => {
+			let _self = this;
+			this.database.allDocs({
+				include_docs: true,
+				attachments: true
+			}).then(function (result) {
+				// handle result
+				_self.myNotes = result.rows;
+				console.log("Results: " + JSON.stringify(_self.myNotes));
+				resolve(_self.myNotes);
+
+			}).catch(function (err) {
+				console.log(err);
+			});
+		});
+	}
+
+}
